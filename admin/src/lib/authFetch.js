96eccc9b02dddfa2818a943c84bfc75e6e471924
Rodrigo Ferreira <rodrigo.slva.ferreira@gmail.com>
2026-08-08 14:36:41 +0100

    some more fixes

--- admin/src/lib/authFetch.js
+++ admin/src/lib/authFetch.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE = import.meta.env.VITE_API_PROXY_TARGET || "";
 
 export default async function authFetch(url, options = {}) {
     const token = localStorage.getItem("token");
